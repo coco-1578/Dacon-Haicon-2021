efb007459b837f76d4aa8760245bc77f08e663d7
coco1578 <rlawnsgh0826@gmail.com>
2021-08-24 14:58:32 +0900

    LSTM Autoencoder 모델 추가

--- src/model.py
+++ src/model.py
@@ -43,3 +43,57 @@ class TransformerModel(nn.Module):
         output = self.transformer_encoder(x, None)
         output = self.decoder(self.relu(output))
         return x[0] + output[-1]  # skip connection
+
+
+class LSTMEncoder(nn.Moudle):
+
+    def __init__(self, n_features, hidden_size, num_layers=2, bidirectional=True, dropout=0):
+        super(LSTMEncoder, self).__init__()
+        self.lstm = nn.LSTM(input_size=n_features,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            bidirectional=bidirectional,
+                            dropout=dropout)
+
+    def forward(self, x):
+
+        x = x.transpose(0, 1)  # (batch, seq, features) -> (seq, batch, features)
+        x_n, (h_n, c_n) = self.lstm(x)
+        # return last output
+        return x[-1]  # (batch, features)
+
+
+class LSTMDecoder(nn.Module):
+
+    def __init__(self, window_size, n_features, hidden_size, num_layers=2, bidirectional=True, dropout=0):
+        super(LSTMDecoder, self).__init__()
+        self.window_size = window_size
+        self.lstm = nn.LSTM(input_size=hidden_size * 2,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            bidirectional=bidirectional,
+                            dropout=dropout)
+        self.fc = nn.Linear(2 * hidden_size if bidirectional else hidden_size, n_features)
+
+    def forward(self, x):
+        # x'shape -> (batch, features) -> repeat
+        x = x.repeat(self.window_size, 1, 1)
+        x_n, (h_n, c_n) = self.lstm(x)
+        output = self.fc(x_n[-1])
+
+        return output
+
+
+class LSTMAE(nn.Module):
+
+    def __init__(self, window_size, n_features, hidden_size, num_layers=2, bidirectional=True, dropout=0):
+        super(LSTMAE, self).__init__()
+
+        self.encoder = LSTMEncoder(n_features, hidden_size, num_layers, bidirectional, dropout)  # hidden_size = 128
+        self.decoder = LSTMDecoder(window_size, n_features, hidden_size * 2, num_layers, bidirectional, dropout)
+
+    def forward(self, x):
+
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
